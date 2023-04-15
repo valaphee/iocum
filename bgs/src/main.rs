63be4fab339015d3354cc00665321b1b82aaa51f
Valaphee <32491319+valaphee@users.noreply.github.com>
2023-04-15 13:38:47 +0200

    Working on patch

--- bgs/src/main.rs
+++ bgs/src/main.rs
@@ -1,110 +1,162 @@
-use std::sync::Arc;
+use std::{fs::File, sync::Arc};
 
+use byteorder::{BigEndian, ReadBytesExt};
 use clap::{arg, Parser};
 use futures_util::{SinkExt, StreamExt};
+use rsa::pkcs1v15::SigningKey;
+use rsa::RsaPrivateKey;
+use rsa::signature::RandomizedSigner;
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 use tokio::net::TcpListener;
 use tokio_rustls::{rustls::ServerConfig, TlsAcceptor};
-use tokio_tungstenite::tungstenite::{client::IntoClientRequest, handshake::server};
+use tokio_tungstenite::tungstenite::{client::IntoClientRequest, handshake::server, Message};
 use url::Url;
 
 use tlsbogie::ResolvesServerCertAutogen;
 
 #[derive(Parser)]
-struct Arguments {
-    #[arg(long)]
-    local_addr: Option<Url>,
-    #[arg(long)]
-    remote_addr: Url,
-    #[arg(long)]
-    default_sni: Option<String>,
+enum Arguments {
+    Mitm {
+        remote_addr: Url,
+        #[arg(long)]
+        local_addr: Option<Url>,
+        #[arg(long)]
+        default_sni: Option<String>,
+    },
+    Patch {
+        file: File,
+    },
 }
 
 #[tokio::main]
 async fn main() {
-    let arguments = Arguments::parse();
-
-    let local_addr = arguments.local_addr.unwrap();
-    let listener = TcpListener::bind(
-        local_addr
-            .socket_addrs(|| Some(1119))
-            .unwrap()
-            .first()
-            .unwrap(),
-    )
-    .await
-    .unwrap();
-
-    // setup tls server config
-    let mut tls_server_config = ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_cert_resolver(Arc::new(ResolvesServerCertAutogen::new(
-            "certs",
-            arguments
-                .default_sni
-                .unwrap_or(local_addr.host().unwrap().to_string()),
-        )));
-    let tls_acceptor = TlsAcceptor::from(Arc::new(tls_server_config));
-
-    loop {
-        let (stream, _) = listener.accept().await.unwrap();
-        let tls_acceptor = tls_acceptor.clone();
-        let remote_addr = arguments.remote_addr.clone();
-
-        tokio::task::spawn(async move {
-            let stream = tls_acceptor.accept(stream).await.unwrap();
-            let mut stream = tokio_tungstenite::accept_hdr_async(
-                stream,
-                |request: &server::Request, mut response: server::Response| {
-                    println!("----BEGIN HTTP REQUEST-----");
-                    println!(
-                        "{} {} {:?}",
-                        request.method(),
-                        request.uri().path_and_query().unwrap().as_str(),
-                        request.version()
-                    );
-                    for (header_name, header_value) in request.headers().iter() {
-                        println!("{}: {}", header_name, header_value.to_str().unwrap());
-                    }
-                    println!("----END HTTP REQUEST-----");
-
-                    response.headers_mut().append(
-                        "sec-websocket-protocol",
-                        "v1.rpc.battle.net".parse().unwrap(),
-                    );
-
-                    Ok(response)
-                },
+    match Arguments::parse() {
+        Arguments::Mitm {
+            remote_addr,
+            local_addr,
+            default_sni,
+        } => {
+            let local_addr = local_addr.unwrap();
+            let listener = TcpListener::bind(
+                local_addr
+                    .socket_addrs(|| Some(1119))
+                    .unwrap()
+                    .first()
+                    .unwrap(),
             )
             .await
             .unwrap();
 
-            let mut request = remote_addr.into_client_request().unwrap();
-            request.headers_mut().append(
-                "sec-websocket-protocol",
-                "v1.rpc.battle.net".parse().unwrap(),
-            );
-            let (mut remote_stream, response) =
-                tokio_tungstenite::connect_async_tls_with_config(request, None, None)
-                    .await
-                    .unwrap();
-            println!("----BEGIN HTTP RESPONSE-----");
-            println!("{:?} {}", response.version(), response.status().as_str());
-            for (header_name, header_value) in response.headers().iter() {
-                println!("{}: {}", header_name, header_value.to_str().unwrap());
-            }
-            println!("----END HTTP RESPONSE-----");
+            // setup tls server config
+            let tls_server_config = ServerConfig::builder()
+                .with_safe_defaults()
+                .with_no_client_auth()
+                .with_cert_resolver(Arc::new(ResolvesServerCertAutogen::new(
+                    "certs",
+                    default_sni.unwrap_or(local_addr.host().unwrap().to_string()),
+                )));
+            let tls_acceptor = TlsAcceptor::from(Arc::new(tls_server_config));
 
             loop {
-                tokio::select! {
-                    message = stream.next() => {
-                        remote_stream.send(message.unwrap().unwrap()).await.unwrap();
+                let (stream, _) = listener.accept().await.unwrap();
+                let tls_acceptor = tls_acceptor.clone();
+                let remote_addr = remote_addr.clone();
+
+                tokio::task::spawn(async move {
+                    let stream = tls_acceptor.accept(stream).await.unwrap();
+                    let mut stream = tokio_tungstenite::accept_hdr_async(
+                        stream,
+                        |request: &server::Request, mut response: server::Response| {
+                            println!("----BEGIN HTTP REQUEST-----");
+                            println!(
+                                "{} {} {:?}",
+                                request.method(),
+                                request.uri().path_and_query().unwrap().as_str(),
+                                request.version()
+                            );
+                            for (header_name, header_value) in request.headers().iter() {
+                                println!("{}: {}", header_name, header_value.to_str().unwrap());
+                            }
+                            println!("----END HTTP REQUEST-----");
+
+                            response.headers_mut().append(
+                                "sec-websocket-protocol",
+                                "v1.rpc.battle.net".parse().unwrap(),
+                            );
+
+                            Ok(response)
+                        },
+                    )
+                    .await
+                    .unwrap();
+
+                    let mut request = remote_addr.into_client_request().unwrap();
+                    request.headers_mut().append(
+                        "sec-websocket-protocol",
+                        "v1.rpc.battle.net".parse().unwrap(),
+                    );
+                    let (mut remote_stream, response) =
+                        tokio_tungstenite::connect_async_tls_with_config(request, None, None)
+                            .await
+                            .unwrap();
+                    println!("----BEGIN HTTP RESPONSE-----");
+                    println!("{:?} {}", response.version(), response.status().as_str());
+                    for (header_name, header_value) in response.headers().iter() {
+                        println!("{}: {}", header_name, header_value.to_str().unwrap());
                     }
-                    message = remote_stream.next() => {
-                        stream.send(message.unwrap().unwrap()).await.unwrap();
+                    println!("----END HTTP RESPONSE-----");
+
+                    loop {
+                        tokio::select! {
+                            message = stream.next() => {
+                                remote_stream.send(message.unwrap().unwrap()).await.unwrap();
+                            }
+                            message = remote_stream.next() => {
+                                stream.send(message.unwrap().unwrap()).await.unwrap();
+                            }
+                        }
                     }
-                }
+                });
             }
-        });
+        }
+        Arguments::Patch { file } => {
+            let file_content = std::fs::read_to_string(file).unwrap();
+            if let Some(certificate_bundle_start) = file_content.find("{\"Created\":\"") {
+                serde_json::to_string(&CertificateBundle {
+                    created: 0,
+                    certificates: vec![],
+                    public_keys: vec![],
+                    signing_certificates: vec![],
+                    root_ca_public_keys: vec![],
+                }).unwrap();
+
+                let mut rng = rand::thread_rng();
+                let key_pair = RsaPrivateKey::new(&mut rng, 2048).unwrap();
+                let signing_key = SigningKey::<Sha256>::new_with_prefix(private_key);
+                let sign = signing_key.sign_with_rng(&mut rng, b"");
+            }
+        }
     }
+}
+
+#[derive(Serialize, Deserialize)]
+struct CertificateBundle {
+    created: u64,
+    certificates: Vec<CertificateBundlePublicKey>,
+    public_keys: Vec<CertificateBundlePublicKey>,
+    signing_certificates: Vec<CertificateBundleCertificate>,
+    root_ca_public_keys: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CertificateBundlePublicKey {
+    uri: String,
+    #[serde(with = "hex")]
+    sha256: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CertificateBundleCertificate {
+    data: String
 }
--- http/src/main.rs
+++ http/src/main.rs
@@ -13,133 +13,134 @@ use tokio_rustls::{
 use tlsbogie::ResolvesServerCertAutogen;
 
 #[derive(Parser)]
-struct Arguments {
-    #[arg(long)]
-    local_addr: Option<Uri>,
-    #[arg(long)]
-    remote_addr: Uri,
-    #[arg(long)]
-    default_sni: Option<String>,
+enum Arguments {
+    Mitm {
+        remote_addr: Uri,
+        #[arg(long)]
+        local_addr: Option<Uri>,
+        #[arg(long)]
+        default_sni: Option<String>,
+    },
 }
 
 #[tokio::main]
 async fn main() {
-    let arguments = Arguments::parse();
+    if let Arguments::Mitm {
+        remote_addr,
+        local_addr,
+        default_sni,
+    } = Arguments::parse()
+    {
+        let local_addr = local_addr.unwrap();
+        let listener = TcpListener::bind(format!(
+            "{}:{}",
+            local_addr.host().unwrap(),
+            local_addr.port().map_or(443, |port| port.as_u16())
+        ))
+        .await
+        .unwrap();
 
-    let local_addr = arguments.local_addr.unwrap();
-    let listener = TcpListener::bind(format!(
-        "{}:{}",
-        local_addr.host().unwrap(),
-        local_addr.port().map_or(443, |port| port.as_u16())
-    ))
-    .await
-    .unwrap();
+        // setup tls server config
+        let mut tls_server_config = ServerConfig::builder()
+            .with_safe_defaults()
+            .with_no_client_auth()
+            .with_cert_resolver(Arc::new(ResolvesServerCertAutogen::new(
+                "certs",
+                default_sni.unwrap_or(local_addr.host().unwrap().to_string()),
+            )));
+        tls_server_config.alpn_protocols = vec![b"h2".to_vec()];
+        let tls_acceptor = TlsAcceptor::from(Arc::new(tls_server_config));
 
-    // setup tls server config
-    let mut tls_server_config = ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_cert_resolver(Arc::new(ResolvesServerCertAutogen::new(
-            "certs",
-            arguments
-                .default_sni
-                .unwrap_or(local_addr.host().unwrap().to_string()),
-        )));
-    tls_server_config.alpn_protocols = vec![b"h2".to_vec()];
-    let tls_acceptor = TlsAcceptor::from(Arc::new(tls_server_config));
+        // setup tls client config
+        let mut root_cert_store = RootCertStore::empty();
+        root_cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
+            |trust_anchor| {
+                OwnedTrustAnchor::from_subject_spki_name_constraints(
+                    trust_anchor.subject,
+                    trust_anchor.spki,
+                    trust_anchor.name_constraints,
+                )
+            },
+        ));
+        let mut tls_client_config = ClientConfig::builder()
+            .with_safe_defaults()
+            .with_root_certificates(root_cert_store)
+            .with_no_client_auth();
+        tls_client_config.alpn_protocols = vec![b"h2".to_vec()];
+        let tls_connector = TlsConnector::from(Arc::new(tls_client_config));
 
-    // setup tls client config
-    let mut root_cert_store = RootCertStore::empty();
-    root_cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(
-        |trust_anchor| {
-            OwnedTrustAnchor::from_subject_spki_name_constraints(
-                trust_anchor.subject,
-                trust_anchor.spki,
-                trust_anchor.name_constraints,
-            )
-        },
-    ));
-    let mut tls_client_config = ClientConfig::builder()
-        .with_safe_defaults()
-        .with_root_certificates(root_cert_store)
-        .with_no_client_auth();
-    tls_client_config.alpn_protocols = vec![b"h2".to_vec()];
-    let tls_connector = TlsConnector::from(Arc::new(tls_client_config));
+        let remote_addr = format!(
+            "{}:{}",
+            remote_addr.host().unwrap(),
+            remote_addr.port().map_or(443, |port| port.as_u16())
+        )
+        .to_socket_addrs()
+        .unwrap()
+        .next()
+        .unwrap();
+        let remote_host = arguments.remote_addr.host().unwrap().to_string();
 
-    let remote_addr = format!(
-        "{}:{}",
-        arguments.remote_addr.host().unwrap(),
-        arguments
-            .remote_addr
-            .port()
-            .map_or(443, |port| port.as_u16())
-    )
-    .to_socket_addrs()
-    .unwrap()
-    .next()
-    .unwrap();
-    let remote_host = arguments.remote_addr.host().unwrap().to_string();
+        loop {
+            let (stream, _) = listener.accept().await.unwrap();
+            let tls_acceptor = tls_acceptor.clone();
+            let tls_connector = tls_connector.clone();
+            let remote_host = remote_host.clone();
 
-    loop {
-        let (stream, _) = listener.accept().await.unwrap();
-        let tls_acceptor = tls_acceptor.clone();
-        let tls_connector = tls_connector.clone();
-        let remote_host = remote_host.clone();
-
-        tokio::task::spawn(async move {
-            let stream = tls_acceptor.accept(stream).await.unwrap();
-            let service = service_fn(move |request: Request<Incoming>| {
-                println!("----BEGIN HTTP REQUEST-----");
-                println!(
-                    "{} {} {:?}",
-                    request.method(),
-                    request.uri().path_and_query().unwrap().as_str(),
-                    request.version()
-                );
-                for (header_name, header_value) in request.headers().iter() {
-                    println!("{}: {}", header_name, header_value.to_str().unwrap());
-                }
-                println!("----END HTTP REQUEST-----");
-
-                let tls_connector = tls_connector.clone();
-                let remote_host = remote_host.clone();
-
-                let (mut parts, body) = request.into_parts();
-                let mut uri_parts = parts.uri.into_parts();
-                uri_parts.authority = Some(Authority::try_from(remote_host.clone()).unwrap());
-                parts.uri = Uri::from_parts(uri_parts).unwrap();
-                let request = Request::from_parts(parts, body);
-
-                async move {
-                    let stream = TcpStream::connect(&remote_addr).await.unwrap();
-                    let stream = tls_connector
-                        .connect(ServerName::try_from(remote_host.as_str()).unwrap(), stream)
-                        .await
-                        .unwrap();
-                    let (mut sender, connection) =
-                        client::conn::http2::handshake(TokioExecutor, stream)
-                            .await
-                            .unwrap();
-                    tokio::task::spawn(async move {
-                        connection.await.unwrap();
-                    });
-
-                    let response = sender.send_request(request).await.unwrap();
-                    println!("----BEGIN HTTP RESPONSE-----");
-                    println!("{:?} {}", response.version(), response.status().as_str());
-                    for (header_name, header_value) in response.headers().iter() {
+            tokio::task::spawn(async move {
+                let stream = tls_acceptor.accept(stream).await.unwrap();
+                let service = service_fn(move |request: Request<Incoming>| {
+                    println!("----BEGIN HTTP REQUEST-----");
+                    println!(
+                        "{} {} {:?}",
+                        request.method(),
+                        request.uri().path_and_query().unwrap().as_str(),
+                        request.version()
+                    );
+                    for (header_name, header_value) in request.headers().iter() {
                         println!("{}: {}", header_name, header_value.to_str().unwrap());
                     }
-                    println!("----END HTTP RESPONSE-----");
+                    println!("----END HTTP REQUEST-----");
 
-                    Ok::<_, Error>(response)
-                }
+                    let tls_connector = tls_connector.clone();
+                    let remote_host = remote_host.clone();
+
+                    let (mut parts, body) = request.into_parts();
+                    let mut uri_parts = parts.uri.into_parts();
+                    uri_parts.authority = Some(Authority::try_from(remote_host.clone()).unwrap());
+                    parts.uri = Uri::from_parts(uri_parts).unwrap();
+                    let request = Request::from_parts(parts, body);
+
+                    async move {
+                        let stream = TcpStream::connect(&remote_addr).await.unwrap();
+                        let stream = tls_connector
+                            .connect(ServerName::try_from(remote_host.as_str()).unwrap(), stream)
+                            .await
+                            .unwrap();
+                        let (mut sender, connection) =
+                            client::conn::http2::handshake(TokioExecutor, stream)
+                                .await
+                                .unwrap();
+                        tokio::task::spawn(async move {
+                            connection.await.unwrap();
+                        });
+
+                        let response = sender.send_request(request).await.unwrap();
+                        println!("----BEGIN HTTP RESPONSE-----");
+                        println!("{:?} {}", response.version(), response.status().as_str());
+                        for (header_name, header_value) in response.headers().iter() {
+                            println!("{}: {}", header_name, header_value.to_str().unwrap());
+                        }
+                        println!("----END HTTP RESPONSE-----");
+
+                        Ok::<_, Error>(response)
+                    }
+                });
+                server::conn::http2::Builder::new(TokioExecutor)
+                    .serve_connection(stream, service)
+                    .await
+                    .unwrap();
             });
-            server::conn::http2::Builder::new(TokioExecutor)
-                .serve_connection(stream, service)
-                .await
-                .unwrap();
-        });
+        }
     }
 }
 
